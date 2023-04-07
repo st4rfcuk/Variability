80ec69e9745dc87f6bd4a8fb17e80c760d7b4daa
st4rfcuk <130094432+st4rfcuk@users.noreply.github.com>
2023-04-07 13:55:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+const canvas = document.createElement("canvas");
+canvas.id = "myCanvas";
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.style.zIndex = 8;
+canvas.style.position = "absolute";
+
+const body = document.getElementsByTagName("body")[0];
+body.appendChild(canvas);
+
+const ctx = canvas.getContext("2d");
+
 function drawArt() {
   var points = [];
   var colors = [];
-  var r1;
-  var r2;
-  var g1;
-  var g2;
-  var b1;
-  var b2;
   var xo, yo;
   var dens_min=2;
   var dens_max=15;
@@ -16,19 +22,20 @@ function drawArt() {
   var background_color_g=0;
   var background_color_b=0;
 
-  background(background_color_r, background_color_g, background_color_b);
+  ctx.fillStyle = `rgb(${background_color_r}, ${background_color_g}, ${background_color_b})`;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   noiseDetail(1);
   angleMode(DEGREES);
-  xo = width / 2;
-  yo = height / 2;
-  map_max = max(width, height);
+  xo = canvas.width / 2;
+  yo = canvas.height / 2;
+  map_max = max(canvas.width, canvas.height);
 
   var original_density = 10;
-  var space = width / original_density;
+  var space = canvas.width / original_density;
 
-  for (var x = 0 - 2 * space; x < width + 2 * space; x += space) {
-    for (var y = 0 - 2 * space; y < height + 2 * space; y += space) {
+  for (var x = 0 - 2 * space; x < canvas.width + 2 * space; x += space) {
+    for (var y = 0 - 2 * space; y < canvas.height + 2 * space; y += space) {
       var d = dist(x, y, xo, yo);
       var dens = map(d, 0, map_max, dens_max, dens_min);
       for (var i = 0; i < dens; i++) {
@@ -39,7 +46,7 @@ function drawArt() {
   }
 
   points.sort(function(a, b) {
-    return dist(b.get_x(), b.get_y(), xo, yo) - dist(a.get_x(), a.get_y(), xo, yo);
+    return dist(b.x, b.y, xo, yo) - dist(a.x, a.y, xo, yo);
   });
 
   var numColors = m4;
@@ -56,30 +63,22 @@ function drawArt() {
     colors.push(color);
   }
 
-  noStroke();
-
-  if (2*frameCount <= points.length) {
-    var max = 2*frameCount;
-  } else {
-    var max = points.length;
-  }
-
-  for (var i = 0; i < max; i++) {
+  for (var i = 0; i < points.length; i++) {
     var groupSize = ceil(points.length / colors.length);
     var groupIndex = floor(i / groupSize);
     var color = colors[groupIndex];
 
-    var r = map(points[i].get_x(), 0, width, color.r1, color.r2);
-    var g = map(points[i].get_y(), 0, height, color.g1, color.g2);
-    var b = map(points[i].get_x(), 0, width, color.b1, color.b2);
-    fill(r, g, b);
+    var r = map(points[i].x, 0, canvas.width, color.r1, color.r2);
+    var g = map(points[i].y, 0, canvas.height, color.g1, color.g2);
+    var b = map(points[i].x, 0, canvas.width, color.b1, color.b2);
+    ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
 
-    var pp = createVector(points[i].get_y()*m0 - yo*m0 - (points[i].get_x()*m1 - xo*m1),-(points[i].get_x()*m2 - xo*m2) - (points[i].get_y()*m3 - yo*m3));
+    var pp = createVector(points[i].y*m0 - yo*m0 - (points[i].x*m1 - xo*m1),-(points[i].x*m2 - xo*m2) - (points[i].y*m3 - yo*m3));
     var pp_angle = pp.heading();
     var fpp = createVector(cos(pp_angle), sin(pp_angle));
     points[i].add(fpp);
-    ellipse(points[i].get_x(), points[i].get_y(), stroke_size);
+    ctx.ellipse(points[i].x, points[i].y, stroke_size);
   }
-  
-  addPixels();
+  triggerPreview();
+
 }
